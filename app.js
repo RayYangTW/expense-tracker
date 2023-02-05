1deb3694c7e06494c388bc980658c284ce4813a7
ray199229 <ray199229@gmail.com>
2023-02-05 23:42:18 +0800

    feat: add record seeder

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
-require("dotenv").config()
+if(process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
 
 const app = express()
 const PORT = process.env.PORT
+
+mongoose.set('strictQuery', true)
+// 也可以這樣寫
+// mongoose.connect(process.env.MONGODB_URI, () => {
+//   console.log("Connected to MongoDB");
+// })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('monogodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello')
---
+++ models/record.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  name:{
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
