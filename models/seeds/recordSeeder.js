639d3799f8d40d0f88ccd29d1cc25fa38166f7c3
ray199229 <ray199229@gmail.com>
2023-02-10 15:42:02 +0800

    modify recordSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,16 +1,43 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const Record = require("../record")
+const User = require("../user")
 const db = require("../../config/mongoose")
 
+const SEED_USER = {
+  email: 'zxc@zxc.zxc',
+  name: 'example',
+  password: 'zxc'
+}
 
 db.once('open', () => {
   console.log("mongodb connected!")
-  for(let i = 0; i < 5; i++){
-    Record.create({ 
-      name:`Record-${i}`,
-      date: `2023-01-${31-i*2}`,
-      category:`house`,
-      amount:`${i * 1000 + 50}`
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => 
+      User.create({
+        email: SEED_USER.email,
+        name: SEED_USER.name,
+        password: hash
+      }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 5 },
+        (_,i) => Record.create({
+          userId,
+          name:`Record-${i}`,
+          date: `2023-01-${31-i*2}`,
+          category:`house`,
+          amount: Math.floor(Math.random()*3000)+1
+        })))
     })
-  }
-  console.log("done!")
+    .then(() => {
+      console.log("done!")
+      process.exit()
+    })
 })
