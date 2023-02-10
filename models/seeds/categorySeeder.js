22f3bc347979a8b487ab3e244a794df2d6dbf5ed
ray199229 <ray199229@gmail.com>
2023-02-10 18:06:58 +0800

    feat: add category seeder

---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,13 @@
+const Category = require("../category")
+const User = require("../user")
+const db = require("../../config/mongoose")
+
+const categoryRawData = require("../category.json").CATEGORY
+
+db.once('open', () => {
+  Category.create(categoryRawData)
+    .then(() => {
+      console.log('category data is created ')
+      process.exit()
+    })
+})
