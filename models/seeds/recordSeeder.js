868cb83603deda8ad236ef48826331377b5b3a99
ray199229 <ray199229@gmail.com>
2023-02-16 18:33:37 +0800

    modify the seeder to generate different categories in seed data

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -5,7 +5,9 @@ if (process.env.NODE_ENV !== 'production') {
 
 const Record = require("../record")
 const User = require("../user")
+const Category = require("../category")
 const db = require("../../config/mongoose")
+const category = require('../category')
 
 const SEED_USER = {
   email: 'zxc@zxc.zxc',
@@ -15,29 +17,36 @@ const SEED_USER = {
 
 db.once('open', () => {
   console.log("mongodb connected!")
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => 
-      User.create({
-        email: SEED_USER.email,
-        name: SEED_USER.name,
-        password: hash
-      }))
-    .then(user => {
-      const userId = user._id
-      return Promise.all(Array.from(
-        { length: 5 },
-        (_,i) => Record.create({
-          userId,
-          name:`Record-${i}`,
-          date: `2023-01-${31-i*2}`,
-          category:"63e73f0bf6c8add2a3812abd",
-          amount: Math.floor(Math.random()*3000)+1
-        })))
+  Category.find()
+    .lean()
+    .then((categoryData) => {
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(SEED_USER.password, salt))
+        .then(hash => 
+          User.create({
+            email: SEED_USER.email,
+            name: SEED_USER.name,
+            password: hash
+          }))
+        .then(user => {
+          const userId = user._id
+          return Promise.all(Array.from(
+            { length: 5 },
+            (_,i) => 
+            Record.create({
+              userId,
+              name:`Record-${i}`,
+              date: `2023-01-${31-i*2}`,
+              category: categoryData[i]._id,
+              amount: Math.floor(Math.random()*3000)+1
+            })))
+        })
+        .then(() => {
+          console.log("done! seed records created, seed user created.")
+          process.exit()
+        })
     })
-    .then(() => {
-      console.log("done! seed records created, seed user created.")
-      process.exit()
-    })
+
+  
 })
